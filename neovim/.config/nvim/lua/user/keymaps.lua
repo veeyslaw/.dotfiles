57eee96c02d72c9150fa0c887994cc0da219701e
Vladislav Beldiman <veeyslaw@gmail.com>
2023-01-15 23:28:15 +0200

    Update keymaps

--- neovim/.config/nvim/lua/user/keymaps.lua
+++ neovim/.config/nvim/lua/user/keymaps.lua
@@ -1,9 +1,20 @@
+-- Remapping only the keybindings in use so that they do not change on updates
+
 local map = vim.keymap.set
+
+-- TODO: Add keybindings for moving lines up and down
+-- TODO: Add keybinding for toggling line wrap
 
 -- Leader key
 
-map("", "<Space>", "<Nop>")
+map("", "<space>", "<nop>")
 vim.g.mapleader = " "
+-- TODO: vim.g.maplocalleader = " " ?
+
+-- Mouse
+
+vim.opt.mouse = ""
+-- vim.opt.mousemodel = extend
 
 -- Modes
 --     normal       = "n"
@@ -19,8 +30,23 @@ vim.g.mapleader = " "
 
 -- Normal mode
 
+--     Reload config
+map("", "<leader>rc", ":so %<cr>")
+
+--     Move left, down, up, right
 map("n", "n", "h")
 map("n", "e", "j")
 map("n", "o", "k")
 map("n", "i", "l")
+
+--     Enter modes
+
+
+-- G mode
+
+
+
+-- Z mode?
+
+
 
