453d312f9c1a33a64e6e961a775d58b7b043f286
Vladislav Beldiman <veeyslaw@gmail.com>
2023-01-15 13:45:11 +0000

    Update nvim keymaps

--- neovim/.config/nvim/lua/user/keymaps.lua
+++ neovim/.config/nvim/lua/user/keymaps.lua
@@ -1,5 +1,24 @@
 local keymap = vim.keymap.set
 
+-- Leader key
+
+keymap("", "<Space>", "<Nop>")
+vim.g.mapleader = " "
+
+-- Modes
+--     normal       = "n"
+--     insert       = "i"
+--     visual       = "v"
+--     visual block = "x"
+--     term         = "t"
+--     command      = "c"
+
+-- Insert mode
+
+
+-- Normal mode
+
+keymap("n", "<leader>t", ":print \"test\"<CR>")
 keymap("n", "n", "h")
 keymap("n", "e", "j")
 keymap("n", "o", "k")
